7322799a011c20963ea03d2d0700ecfaebd09ec3
Ahtisham Ahmed <ahtishamahmed1234@gmail.com>
2023-09-01 00:21:58 +0500

    TEST

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const nav_bar = document.querySelector('.navigation-bar');
-const nav_button = document.querySelector('.navigation-buttons');
-const hamburger = document.querySelector('#menu-icon');
+let nav_button = document.querySelector('.navigation-buttons');
+let hamburger = document.querySelector('#menu-icon');
 window.addEventListener('scroll', ()=>{
     nav_bar.classList.toggle('scroll-active', window.scrollY > 0)});
 
 hamburger.onclick = () => {
+    hamburger.classList.toggle('bx-x');
     nav_button.classList.toggle('mobile-view');
 }
