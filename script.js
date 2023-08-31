4e2fe6a4600bc5a39cb6c55f64aca1badf4aa99f
Notahti123 <ahtishamahmed1234@gmail.com>
2023-08-31 19:50:04 +0500

    Responsiveness Update

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const nav_bar = document.querySelector('.navigation-bar');
-
+const nav_button = document.querySelector('.navigation-buttons');
+const hamburger = document.querySelector('#menu-icon');
 window.addEventListener('scroll', ()=>{
     nav_bar.classList.toggle('scroll-active', window.scrollY > 0)});
+
+hamburger.onclick = () => {
+    nav_button.classList.toggle('mobile-view');
+}
